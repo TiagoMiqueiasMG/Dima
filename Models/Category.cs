356b2dfe8c5c673eac2dd0af934710a2c33e4f84
TiagoMiqueiasMG <tiagomiqueiasrodrigues@gmail.com>
2024-11-27 21:59:04 -0300

    Starting Dima project

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace Dima.Core.Models;
+
+public class Category
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+}
